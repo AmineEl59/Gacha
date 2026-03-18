adc5674643c49c700c152fce1e5dac7905e03c59
AmineEl59 <a.elbarkaouipro@gmail.com>
2026-03-18 11:58:26 +0100

    fix: CORS allowedOriginPatterns(*) for all APIs

--- invocationapi/src/main/java/com/example/invocationapi/config/SecurityConfig.java
+++ invocationapi/src/main/java/com/example/invocationapi/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8083"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
